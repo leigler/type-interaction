3c29d3e27d2409f7c791c115d1cb582f1a067b16
Rahul S. Shinde <rshinde@mica.edu>
2018-08-22 18:32:11 -0400

    arrow navigation

--- assets/main.js
+++ assets/main.js
@@ -2,8 +2,35 @@ Site = {}
 
 $(document).ready(function(){
   Site.current_slide = 1;
+  Site.total_slides = parseInt($('#presentation_wrapper').data('count'))
   $('.slide_navigation').on('click', Site.jumpSlide)
+  $(window).on('keydown', Site.navigateSlides);
 });
+
+Site.navigateSlides = function(){
+  console.log('hello');
+  if (event.keyCode == 37){
+    Site.slidePrev();
+  } else if (event.keyCode == 39){
+    Site.slideNext();
+  }
+}
+
+Site.slidePrev = function(){
+  Site.current_slide = Site.current_slide - 1;
+  if (Site.current_slide < 1){
+    Site.current_slide = Site.total_slides
+  }
+  Site.renderNewSlide();
+}
+
+Site.slideNext = function(){
+  Site.current_slide = Site.current_slide + 1;
+  if (Site.current_slide > Site.total_slides){
+    Site.current_slide = 1;
+  }
+  Site.renderNewSlide();
+}
 
 Site.jumpSlide = function(){
   Site.current_slide = $(this).data('slide');
